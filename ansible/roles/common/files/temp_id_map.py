f33357f207e9482b0c299211721a55d179208c9e
Daniel Edholm <daniel.edholm91@hotmail.com>
2022-08-11 21:35:44 +0200

    Only include relevant files

--- ansible/roles/common/files/temp_id_map.py
+++
@@ -1,4 +0,0 @@
-# For some reason Ansible could not accept the file name extension ('.py'), so file is intentionally stored w/o ext.
-with open('./temp_id_map', 'w') as f:
-    for uid in range(1000, 65536):
-        f.write("%d:%d:65536\n" %(uid,uid*65536))
