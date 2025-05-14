a24d9c5485e2b65968291573eea0698b47f1ac8a
Daniel Edholm <daniel.edholm@zptcorp.com>
2025-05-14 14:16:58 +0200

    Make tofu initializable again

--- tofu/providers.tf
+++ tofu/providers.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
+      configuration_aliases = [ proxmox.lxc ]
       version = "2.9.14"
     }
     ansible = {
