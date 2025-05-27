40cbe4cdf616afa9dc90425a512910fc65a8daa8
Daniel Edholm <daniel.edholm@zptcorp.com>
2025-05-28 00:32:59 +0200

    Add physical host as hostvar to ansible inventory

--- tofu/modules/proxmox-container/inventory.tf
+++ tofu/modules/proxmox-container/inventory.tf
@@ -2,8 +2,9 @@ resource "ansible_host" "debug" {
   for_each = var.configuration
 
   name      = join(".", [each.key, var.domain])
-  groups    = [ "proxmox_containers" ]
+  groups    = [ each.value.host ]
   variables = {
+    host            = jsonencode(each.value.host)
     roles           = jsonencode(join(",", concat(["proxmox/lxc"], try(each.value.roles, []))))
     services        = jsonencode(each.value.services)
     docker_services = jsonencode(each.value.docker_services)
