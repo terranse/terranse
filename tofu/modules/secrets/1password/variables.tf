63481f0db3e8aefd44cb00f3942067d283b34954
Daniel Edholm <daniel.edholm@zptcorp.com>
2025-09-11 23:07:16 +0200

    feat(secrets): Move 1password provider into plugin

---
+++ tofu/modules/secrets/1password/variables.tf
@@ -0,0 +1,11 @@
+variable "account" {
+  type = string
+}
+
+variable "vault" {
+  type = string
+}
+
+variable "item" {
+  type = string
+}
--- tofu/secrets.tf
+++ tofu/secrets.tf
@@ -1,37 +1,7 @@
-# Taken from https://github.com/1Password/terraform-provider-onepassword/issues/117#issuecomment-2135001654
-data onepassword_vault ops {
-  uuid = "f4h63aecdn7rpzx4sbzyw35jee"
-}
-data onepassword_item service {
-  vault = split("/", data.onepassword_vault.ops.id)[1]
-  title = "Proxmox"
-}
+module "proxmox_secrets" {
+  source  = "./modules/secrets/1password/"
 
-locals {
-  excluded_keys = ["section", "id", "vault", "category"]
-
-  # Get all non-empty top-level fields
-  top_level_fields = {
-    for key, value in data.onepassword_item.service :
-      key => value
-    if !contains(local.excluded_keys, key) &&
-       value != null &&
-       value != "" &&
-       !(try(length(value), -1) == 0)  # Exclude empty lists
-  }
-
-  # Section fields - also filter out empty values here
-  section_fields = merge([
-    for section in try(data.onepassword_item.service.section, []) : {
-      for field in section.field :
-        field.label => field.value
-      if field.value != null && field.value != ""
-    }
-  ]...)
-
-  # Merge both
-  proxmox_secrets = merge(
-    local.top_level_fields,
-    local.section_fields
-  )
+  account = "NYDLBZ4TCZARLJQURRIVNK3RZM"
+  vault = "f4h63aecdn7rpzx4sbzyw35jee"
+  item = "Proxmox"
 }
